aacb145157931504340e61869a85bf821b30ccf8
MarilynPapadopoulos <marilyn.volk@gmail.com>
2021-06-16 14:42:46 -0400

    added ability to comment on pizzas

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -7,10 +7,10 @@ router.route('/:pizzaId').post(addComment);
 // /api/comments/<pizzaId>/<commentId>
 router.route('/:pizzaId/:commentId').delete(removeComment);
 
-const commentRoutes = require('./comment-routes');
-const pizzaRoutes = require('./pizza-routes');
+// const commentRoutes = require('./comment-routes');
+// const pizzaRoutes = require('./pizza-routes');
 
-router.use('/comments', commentRoutes);
-router.use('/pizzas', pizzaRoutes);
+// router.use('/comments', commentRoutes);
+// router.use('/pizzas', pizzaRoutes);
 
 module.exports = router;
