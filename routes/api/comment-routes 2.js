b046eb05d11f407e353fa9c3fed2d2e502d2f3bc
MarilynPapadopoulos <marilyn.volk@gmail.com>
2021-06-22 13:23:52 -0400

    Deploying

---
+++ routes/api/comment-routes 2.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const { addComment, removeComment, addReply, removeReply } = require('../../controllers/comment-controller');
+
+// /api/comments/<pizzaId>
+router.route('/:pizzaId').post(addComment);
+
+// /api/comments/<pizzaId>/<commentId>
+router
+    .route('/:pizzaId/:commentId')
+    .put(addReply)
+    .delete(removeComment);
+
+//delete reply
+router.route('/:pizzaId/commentId/:replyId').delete(removeReply);    
+
+
+// const commentRoutes = require('./comment-routes');
+// const pizzaRoutes = require('./pizza-routes');
+
+// router.use('/comments', commentRoutes);
+// router.use('/pizzas', pizzaRoutes);
+
+module.exports = router;
